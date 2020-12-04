34f05ca47c27aa702db49d4ff72744f04b24aee5
filiperecharte <filipeteixeira00@hotmail.com>
2020-12-04 10:34:50 +0000

    done page frontend

---
+++ pet_add.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+include_once('database/users.php');
+
+
+include_once('templates/common/header.php');
+
+include('templates/common/navigation_bar.php');
+include('templates/forms/pet_add.php');
+include('templates/common/footer.php');
---
+++ templates/forms/pet_add.php
@@ -0,0 +1,54 @@
+<div class="content">
+    <section id="pet_add">
+        <section id="left">
+            <h2>Give us the details about your pet!</h2>
+        </section>
+        <section id="right">
+            <form action="" method="post">
+                <label>Name
+                    <input type="text" name="Name" placeholder="Pet's name">
+                </label>
+                <label>Breed
+                    <input type="text" name="Breed" placeholder="Pet's breed">
+                </label>
+                <label>
+                    <span class="material-icons">
+                    add_photo_alternate
+                    </span>
+                    Choose a photo
+                    <input hidden type="file" name="Photo" id="Pet-photo">
+                </label>
+                <label>Size
+                    <input type="number" name="Size" placeholder="Pet's size">
+                </label>
+                <label>Color
+                    <input type="text" name="color" placeholder="Pet's color">
+                </label>
+                <label>Age
+                    <input type="number" name="age" placeholder="Pet's age">
+                </label>
+                <label>Location
+                    <input type="text" name="location" placeholder="Pet's location">
+                </label>
+                <input type="submit" class="button" id="addButton" value="Add Pet!">
+                <div id="gender-option">
+                    <label>Male
+                        <input type="radio"  name="gender" value="male" checked="checked">
+                        <span class="checkmark"></span>
+                    </label>
+                    <label>Female
+                        <input type="radio"  name="gender" value="female">
+                        <span class="checkmark"></span>
+                    </label>
+                </div>
+                <label>Information
+                    <textarea id="petInfo" name="information" placeholder="Pet's info"></textarea>
+                </label>
+                <select name="status">
+                    <option value="" disabled selected>Status</option>
+                    <option value="">prepared</option>
+                </select>
+            </form>
+        </section>
+    </section>
+</div>
