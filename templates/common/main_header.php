ce04f9b30fc0ed2132e8aa999c67d759dde9a859
filiperecharte <filipeteixeira00@hotmail.com>
2020-12-11 02:13:45 +0000

    homepage responsive

--- templates/common/main_header.php
+++ templates/common/main_header.php
@@ -17,5 +17,26 @@
         <a href="action_logout.php">Logout</a>
     <?php } ?>
     </nav>
-
 </header>
+
+<script src="scripts/hamburger.js">
+</script>
+<div class="hamburger">
+    <!-- Navigation links (hidden by default) -->
+    <div id="myLinks">
+        <a href="pets_list.php">Pets</a>
+        <a href="shelters_list.php">Shelters</a>
+        <?php if (!isset($_SESSION['username'])) { ?>
+            <a href="register.php">Register</a>
+            <a href="login.php">Login</a>
+        <?php } else { ?>
+            <a href="pet_add.php">Add Pet</a>
+            <a href=""><?= getUserByUsername($_SESSION['username'])['name'] ?></a>
+            <a href="action_logout.php">Logout</a>
+        <?php } ?>
+    </div>
+
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+    </a>
+</div>
