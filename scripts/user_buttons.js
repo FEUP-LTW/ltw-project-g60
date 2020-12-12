c444cc9eae9c4947a45ff73c47bdb424e5d244cb
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2020-12-12 17:56:10 +0000

    html and style for editing shelter info -> needs php

--- scripts/user_buttons.js
+++ scripts/user_buttons.js
@@ -14,3 +14,20 @@ function editUserInfo() {
         aboutme.style.display = "none"
     }
 }
+
+function editShelterInfo() {
+    let checkBox = document.getElementById("about_me_check")
+    let icon = document.getElementById("abt_icon")
+    let pets = document.getElementsByClassName("shelter_adoption")[0]
+    let aboutme = document.getElementsByClassName("edit_shelter_info")[0]
+
+    if (checkBox.checked){
+        icon.className = "fas fa-file"
+        pets.style.display = "none"
+        aboutme.style.display = "block"
+    } else {
+        icon.className = "far fa-file"
+        pets.style.display = "block"
+        aboutme.style.display = "none"
+    }
+}
