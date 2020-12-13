951e997b839cd4f2cd8fb9b897ea2a55577a6608
filiperecharte <filipeteixeira00@hotmail.com>
2020-12-13 18:38:54 +0000

    done edit profiles

---
+++ action_edit_shelter.php
@@ -0,0 +1,12 @@
+<?php
+session_start();                         // starts the session
+include_once('database/connection.php'); // connects to the database
+include_once('database/users.php');      // loads the functions responsible for the users table
+include_once('database/upload.php');
+
+editUser($_POST['name'], $_POST['username'], $_POST['password'], "shelter", $_FILES['profile_photo'], $_FILES['header_photo'], null);
+
+$_SESSION['username'] = $_POST['username'];            // store the username
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+
+die();
