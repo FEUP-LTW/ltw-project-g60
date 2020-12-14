22ac36e5920d15ab45954f1923fd56051945b88e
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2020-12-14 16:49:05 +0000

    fixed a minor bug

--- templates/pets/pets_list.php
+++ templates/pets/pets_list.php
@@ -54,7 +54,7 @@
                     </div>
                 </div>
                 <div class="pet-links">
-                    <?php if (isUser($_SESSION['username'])) { ?>
+                    <?php if (isset($_SESSION['username']) and isUser($_SESSION['username'])) { ?>
                     <button id="pet_favorite_button" name="favorite" data-petid="<?= $pet['pet_id'] ?>">Add to Favorites</button>
                     <?php } ?>
                     <?php
